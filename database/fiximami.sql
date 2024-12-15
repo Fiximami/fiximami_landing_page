1ffdf01475bd20e9e8ed96e03c11714a901305a4
Cyril_byte <115986639+cyrilbyte@users.noreply.github.com>
2024-12-15 23:14:35 +0000

    Update fiximami.sql

--- database/fiximami.sql
+++ database/fiximami.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2024 at 01:11 PM
+-- Generation Time: Dec 16, 2024 at 12:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,8 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` int(20) NOT NULL,
+  `role` enum('artisan','user') NOT NULL,
   `email` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,8 +40,14 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `created_at`) VALUES
-(1, 'Cyril', 'Angnumbaala', 'cyrilangnumbaala@gmail.com', '2024-11-29 09:59:42');
+INSERT INTO `users` (`id`, `name`, `phone`, `role`, `email`, `created_at`) VALUES
+(1, 'Angnumbaala', 0, 'artisan', 'cyrilangnumbaala@gmail.com', '2024-11-29 09:59:42'),
+(2, 'Cyril James', 209798956, 'user', 'cy@gmail.com', '2024-12-15 22:28:46'),
+(4, 'Cyril Angnumbaala', 558446518, 'artisan', 'cyrilangnumb@gmail.com', '2024-12-15 22:32:01'),
+(5, 'CKT-UTAS', 558446518, 'user', 'cyrila@gmail.com', '2024-12-15 22:33:25'),
+(7, 'Cyril Success Angnumbaala', 558446518, 'artisan', 'cyrilangnuaala@gmail.com', '2024-12-15 22:43:16'),
+(8, 'jui iko', 987654321, 'user', 'jui@gmail.com', '2024-12-15 22:43:44'),
+(9, 'Jack Chan', 896846453, 'artisan', 'kungfu@gmail.com', '2024-12-15 22:57:26');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
